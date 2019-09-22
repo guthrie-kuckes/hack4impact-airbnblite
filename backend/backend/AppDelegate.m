7964939243e8fe060198c6746660cc183724d158
Guthrie Kuckes <gkuckes@bu.edu>
2019-09-21 23:01:49 -0400

    Added database helper methods

--- backend/backend/AppDelegate.m
+++ backend/backend/AppDelegate.m
@@ -19,9 +19,29 @@
 {
     NSString* name = [property valueForKey:@"name"];
     NSNumber* max_occupancy = [property valueForKey:@"max_occupancy"];
-    NSString* formatted = [NSString stringWithFormat:@"name: \"%@\", max_occupancy: %@",
-                           name, max_occupancy];
+    NSNumber* max_days = [property valueForKey:@"max_days"];
+    NSNumber* nightly_cost = [property valueForKey:@"nightly_cost"];
+    NSString* reservedString = [property valueForKey:@"reserved"] ? @"true" : @"false";
+    
+    NSString* formatted = [NSString stringWithFormat:@"name: \"%@\", max_occupancy: %@, max_days: %@, nightly_cost: %@ cents, reserved: %@",
+                           name, max_occupancy, max_days, nightly_cost, reservedString];
     NSLog(formatted);
+}
+
+-(void)logWholeDatabase
+{
+    NSError *error = nil;
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"H4IRentalProperty"];
+    NSArray *results = [self.objectContext executeFetchRequest:request error:&error];
+    for (NSManagedObject* obj in results)
+    {
+        [AppDelegate logProperty:obj];
+    }
+    if (!results) {
+        NSLog(@"Error fetching Employee objects: %@\n%@", [error localizedDescription], [error userInfo]);
+        abort();
+    }
+
 }
 
 
@@ -57,6 +77,8 @@
 
 -(void)addPropertyToDatabase:(NSString*)propertyName
             withMaxOccupancy:(uint16)maxOccupancy
+            maxDaysForRental:(uint16)maxDays
+                 nightlyCost:(uint16)cost
                        error:(NSError**)error
 {
     NSManagedObject* newOne = [NSEntityDescription insertNewObjectForEntityForName:@"H4IRentalProperty"
@@ -64,6 +86,15 @@
     [newOne setValue:propertyName forKey:@"name"];
     NSNumber* copy = [NSNumber numberWithInteger:maxOccupancy];
     [newOne setValue:copy forKey:@"max_occupancy"];
+    
+    copy = [NSNumber numberWithInteger:maxDays];
+    [newOne setValue:copy forKey:@"max_days"];
+
+    copy = [NSNumber numberWithInteger:cost];
+    [newOne setValue:copy forKey:@"nightly_cost"];
+    
+    [newOne setValue:FALSE forKey:@"reserved"];
+    
     [self.objectContext save:error];
 }
 
@@ -76,34 +107,21 @@
     [self initializeDatabase];
     //[self deleteDatabase];
     
+    [self logWholeDatabase];
     
-    NSError* addError = nil;
-    [self addPropertyToDatabase:@"my mansion" withMaxOccupancy:5000 error:&addError
+   /* NSError* addError = nil;
+    [self addPropertyToDatabase:@"my mansion" withMaxOccupancy:5000 maxDaysForRental:-5 nightlyCost: 100*100 error:&addError
      ];
     if (addError)
-        NSLog(addError.description);
+        NSLog(addError.description);*/
     
-    NSError *error = nil;
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"H4IRentalProperty"];
-    NSArray *results = [self.objectContext executeFetchRequest:request error:&error];
-    for (NSManagedObject* obj in results)
-    {
-        [AppDelegate logProperty:obj];
-    }
-    if (!results) {
-        NSLog(@"Error fetching Employee objects: %@\n%@", [error localizedDescription], [error userInfo]);
-        abort();
-    }
     
     [self.server addBlock:^(CRRequest * _Nonnull request, CRResponse * _Nonnull response, CRRouteCompletionBlock  _Nonnull completionHandler) {
         [response setValue:[NSBundle mainBundle].bundleIdentifier forHTTPHeaderField:@"Server"];
         completionHandler();
     }];
     
-    /*[self.server get:@"/" block:^(CRRequest * _Nonnull request, CRResponse * _Nonnull response, CRRouteCompletionBlock  _Nonnull completionHandler) {
-        [response sendString:@"Hello world!"];
-        completionHandler();
-    }];*/
+    
     [self.server addBlock:^(CRRequest * _Nonnull request, CRResponse * _Nonnull response, CRRouteCompletionBlock  _Nonnull completionHandler) {
     
         NSString* serve = [NSString stringWithContentsOfFile:@"/Users/valence/Desktop/index.html" encoding:NSUTF8StringEncoding error:nil];
